57ed3ef683557cbd2ec054ca9bc89920957c72e1
Danil Agafonov <danil.agafonov@flatstack.com>
2019-03-08 00:53:46 +0300

    Implement Reward API request

--- src/reward-api/reward-api.service.ts
+++ src/reward-api/reward-api.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import * as http from 'http';
 
 @Injectable()
 export class RewardApiService {
-  request() {
-
+  request(options, callback) {
+    return http.request(options, callback);
   }
 }
